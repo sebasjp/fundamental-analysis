0dea14d338df64bd547095113e448ae0057c608c
sebasjp <sebasjimenez.parra@gmail.com>
2024-12-09 05:58:31 -0500

    inicio proceso de valorizacion de empresas

---
+++ docker/utils.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+COLOR_OFF="\033[0m"
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+BROWN="\033[0;33m"
+BROWN="\033[0;34m"
+
+if [ $1 == '--start_dev' ]
+then
+
+    printf "${GREEN}Starting development environment${COLOR_OFF}\n"
+    docker compose -f docker-compose.yaml up -d --build
+    sudo chmod -R go+w ../
+
+elif [ $1 == '--close_dev' ]
+then
+
+    printf "${RED}Closing development environment${COLOR_OFF}\n"
+    docker compose -f docker-compose.yaml down
+
+elif [ $1 == '--help' ]
+then
+    printf "${GREEN}Options:
+        --start_dev Start development environment
+        --close_dev Close development environment
+        ${COLOR_OFF}\n"
+
+else
+    printf "${RED}Error: Invalid option. Type '$0 --help' for avaliable options\n"
+    exit 1
+fi
